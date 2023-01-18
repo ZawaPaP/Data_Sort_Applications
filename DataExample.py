7c6af86888eedd3e086ca0569cfcf65cc8db9e66
Kazuhide Izawa <kazuhide@fb.com>
2023-01-18 14:15:00 +0800

    removed the old file

--- DataExample.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def data_example():
-    data = [random.randint(0,100) for i in range(100)]
-    print("this is example data randomly generated")
-    print(data)
-    return data
--- FileReader.py
+++
@@ -1,9 +0,0 @@
-class FileReader():
-    def __init__(self, args):
-        self.args = args
-        self.fp = args.input_file_path
-
-    def read(self):
-        data = self.fp.read() #ここ失敗した時の回避エラーを入れる
-        self.fp.close()
-        return data
